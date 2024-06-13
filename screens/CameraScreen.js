0eae74943b86aa756115937ecd5c68c502850dbc
Trombi <112467491+TrombiFox@users.noreply.github.com>
2024-06-13 18:08:37 -0400

    getting somewhere with camera

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -3,14 +3,111 @@
 // initial code taken from documentation:
 // https://docs.expo.dev/versions/latest/sdk/camera/#types
 
+import { CameraView, useCameraPermissions, takePic } from 'expo-camera';
+import React, { useEffect, useState, useRef } from 'react';
+import { Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
+// import * as MediaLibray from 'expo-media-library';
+import { AntDesign } from '@expo/vector-icons';
 
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+const CameraScreen = ({ route, navigation }) => {
 
-export default function App() {
+  // const [cameraRef, setCameraRef] = useState();
+  let cameraRef = useRef();
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [photo, setPhoto] = useState();
+
+  
+
+
+  // const [mediaPermission, setMediaPermission] = useState();
+
+
+  //  // maybe later
+  // useEffect => {
+  //   (async () => {
+  //     const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+  //   }) ();
+  // }, []);
+
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          style={styles.navTouchStyle}
+          onPress={() => {
+            navigation.navigate(
+              'Add Item'
+            );
+            console.log('headerLeft (Add Item) clicked!');
+          }}
+        >
+          <AntDesign name="left" size={24} color="black" />
+          <Text style={styles.navTextStyle}> Cancel </Text>
+        </TouchableOpacity>
+      ),
+    })
+    // things to do
+  }, []);
+
+
+
+  
+
+  let takePic = async() => {
+    let options={
+      quality: 1,
+      base64: true,
+      exif: false
+    };
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+    // console.log(newPhoto)
+    // console.log(photo)
+    console.log('end of setting newPhoto');
+  };
+
+
+
+
+  if (photo) {
+    let savePhoto = () => {
+      // navigate(
+      //   'Add Item',
+      //     (photo.uri)
+      // )
+      setPhoto(undefined);
+    };
+    return(
+      <View styles={styles.previewContainer}>
+        <Text> asshole </Text>
+        <Image
+          style={styles.imagePreview}
+          source={{ uri: 'data:image/jpg;base64,' + photo.base64}}
+        />
+        <Button
+          title='Use Photo'
+          onPress={savePhoto}
+        />
+        <Button
+          title='Discard Photo'
+          onPress={() => {
+            setPhoto(undefined)
+          }}
+        />
+      </View>
+    );
+  }
+
+
+
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -33,10 +130,16 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={takePic}
+          >
+            <Text style={styles.text}> Take Picture </Text>
           </TouchableOpacity>
         </View>
       </CameraView>
@@ -48,6 +151,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  previewContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   camera: {
     flex: 1,
@@ -68,4 +176,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  imagePreview: {
+    alignSelf: 'stretch',
+    flex: 1,
+  },
+  navTextStyle: {
+    fontSize: 15,
+  },
+  navTouchStyle: {
+    flexDirection: 'row',
+    justifyContet: 'flex-end',
+    alignItems: 'center',
+  },
 });
+
+export default CameraScreen;
